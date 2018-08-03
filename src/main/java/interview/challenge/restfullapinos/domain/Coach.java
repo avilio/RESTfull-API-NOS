3a431007b81e6d4041420364890a380e841d4cbb
claudiomonteiroalmeida <cmmonteiroalmeida@gmail.com>
2018-08-03 20:09:55 +0100

    Domain Model Created Close #1

---
+++ src/main/java/interview/challenge/restfullapinos/domain/Coach.java
@@ -0,0 +1,19 @@
+package interview.challenge.restfullapinos.domain;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document
+public class Coach {
+
+    @Id
+    private String id;
+    @Indexed
+    private String name;
+
+    private Team team;
+
+}
---
+++ src/main/java/interview/challenge/restfullapinos/domain/Team.java
@@ -0,0 +1,21 @@
+package interview.challenge.restfullapinos.domain;
+
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document
+public class Team {
+
+    @Id
+    private String id;
+    @Indexed
+    private String name;
+
+    private Coach coach;
+
+
+}
